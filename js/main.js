bebb22189322b25554cbbbb2543640d4e1447d6c
moeamaya <moeamaya@gmail.com>
2013-04-22 15:30:52 -0400

    from macbook

--- js/main.js
+++ js/main.js
@@ -1,10 +1,37 @@
+$(window).load(function(){
 
-var machines = $('.options').find('li')
 
-console.log(machines)
-
-$(machines).click( function(){
-    $.each($(this), function(el){
-        $(el).removeClass('selected')
+    var machines = $('.options').find('li');
+    
+    $(machines).click( function(){
+	choice( $(this) )
     })
+
+    
+    //select helper function
+    function choice(el){
+	$(el).siblings().each( function(){
+	  $(this).removeClass('selected');
+          $(this).find('span').html('')
+	})
+	
+	$(el).addClass('selected');
+        $(el).find('span').text('>')
+        
+        if ($(el).attr('id') == '60'){
+            var title = '60 Watt Epilog'
+        }
+        if ($(el).attr('id') == '120'){
+            var title = '120 Watt Epilog'
+        }
+        if ($(el).attr('id') == 'univ'){
+            var title = 'Universal'
+        }
+        
+        $('#right').find('h2').text(title)
+    }
+    
+
+
 })
+
