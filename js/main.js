5fca48071f04fca9a8a717a8232899f63cda6ea3
moeamaya <moeamaya@gmail.com>
2013-05-10 16:10:29 -0400

    ajax signup

--- js/main.js
+++ js/main.js
@@ -38,7 +38,43 @@ $(window).load(function(){
         $('#right').find('h2').text(title)
     }
     
+    jQuery(function ($) {
+	$('#right table td').click(function (e) {
+		e.preventDefault();
 
+		// example of calling the confirm function
+		// you must use a callback function to perform the "yes" action
+		confirm("Sign up for laser?", function () {
+			window.location.href = 'http://simplemodal.com';
+		});
+	});
+    });
+    
+    function confirm(message, callback) {
+	    $('#confirm').modal({
+		    closeHTML: "<a href='#' title='Close' class='modal-close'>x</a>",
+		    position: ["20%",],
+		    overlayId: 'confirm-overlay',
+		    containerId: 'confirm-container', 
+		    onShow: function (dialog) {
+			    var modal = this;
+    
+			    $('.message', dialog.data[0]).append(message);
+    
+			    // if the user clicks "yes"
+			    $('.yes', dialog.data[0]).click(function () {
+				    // call the callback
+				    if ($.isFunction(callback)) {
+					    callback.apply();
+				    }
+				    // close the dialog
+				    modal.close(); // or $.modal.close();
+			    });
+		    }
+	    });
+}
+    
+    
 
 })
 
